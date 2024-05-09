dd0b5812d3d8b409b4f1c44a18f0ca9f746d8bfe
arifraj199 <karif9514@gmail.com>
2024-05-10 04:06:39 +0600

    blog post api implement

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
 
-// arifraj9911
-// br7dnzmb6dOXh658
-
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    withCredentials: true,
+  })
+);
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcfrddp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,16 +24,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const sportsCollection = client.db("dbBlog").collection("blog");
+
+    app.get("/blogs", async (req, res) => {
+      const result = await sportsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      console.log(blog);
+      const result = await sportsCollection.insertOne(blog);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,12 +58,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("sports server is sunning");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send('sports server is sunning')
-})
-
-app.listen(port,()=>{
-    console.log(`sports server running on the port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`sports server running on the port ${port}`);
+});
