f8ab29a0ab7071ddc3ed2739460e4c58a37afff3
arifraj199 <karif9514@gmail.com>
2024-05-10 18:04:40 +0600

    update put api added

--- index.js
+++ index.js
@@ -47,6 +47,40 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/update", async (req, res) => {
+      const result = await sportsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await sportsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateBlogsData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedBlogs = {
+        $set: {
+          title: updateBlogsData.title,
+          image: updateBlogsData.image,
+          category: updateBlogsData.category,
+          short_description: updateBlogsData.short_description,
+          long_description: updateBlogsData.long_description,
+        },
+      };
+      const result = await sportsCollection.updateOne(
+        filter,
+        updatedBlogs,
+        options
+      );
+      res.send(result);
+    });
+
     app.post("/blogs", async (req, res) => {
       const blog = req.body;
       console.log(blog);
